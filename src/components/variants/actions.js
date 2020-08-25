e3e018dbf0b0e337987469faeefb645e3fbe72fe
Alexey Radziuk <radyukag@yandex.ru>
2020-08-25 14:57:47 +0300

    feat: add default information to bird info block

--- src/components/variants/actions.js
+++ src/components/variants/actions.js
@@ -6,6 +6,7 @@ const new_round = createAction('new_round');
 const set_answer = createAction('SET_ANSWER');
 const set_round_number = createAction('SET_ROUND_NUMBER');
 const set_active_bird = createAction('SET_ACTIVE_BIRD');
+const remove_active_bird = createAction('remove_active_bird');
 const set_round_data = createAction('SET_ROUND_DATA');
 
-export { right_answer, wrong_answer, set_answer, set_round_number, set_active_bird, set_round_data, new_round };
+export { right_answer, wrong_answer, set_answer, set_round_number, set_active_bird, set_round_data, new_round, remove_active_bird };
--- src/components/variants/reducers.js
+++ src/components/variants/reducers.js
@@ -1,12 +1,18 @@
-import { right_answer, wrong_answer, set_answer, set_round_number, set_active_bird, set_round_data, new_round } from './actions';
+import { right_answer, wrong_answer, set_answer, set_round_number, set_active_bird, set_round_data, new_round, remove_active_bird } from './actions';
 import { createReducer } from '@reduxjs/toolkit';
+import defaultBird from '../../img/question-bird.jpg';
 
-const defaultState = {
+const defaultRoundState = {
   score: 5,
   roundEnd: false,
 }
 
-const chooseVariantsReducer = createReducer(defaultState, {
+const defaultActiveBirdData = {
+  image: defaultBird,
+  description: "Прослушайте запись и угадайте птицу по пению",
+}
+
+const chooseVariantsReducer = createReducer(defaultRoundState, {
   [wrong_answer]: (state) => {
     return {
       ...state,
@@ -35,8 +41,9 @@ const updateRoundNumberReducer = createReducer(0, {
   [set_round_number]: (state) => state < 5 ? state + 1 : 0,
 });
 
-const setActiveBirdReducer = createReducer('', {
+const setActiveBirdReducer = createReducer(defaultActiveBirdData, {
   [set_active_bird]: (state, action) => action.payload,
+  [remove_active_bird]: () => defaultActiveBirdData,
 });
 
 const setRoundDataReducer = createReducer('', {
